b0d78a8dd888eef9f99b29a8a38eb90b6e2f06d8
Ismael Baira <i.bairao@gmail.com>
2024-10-11 22:35:55 -0700

    Treesitter config from lazy.lua

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -20,7 +20,27 @@ local plugins = {
 	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
 	{ "mhinz/vim-startify" }, -- Single Vimscript plugin
 	{ "nvim-lualine/lualine.nvim" },
-	{ "nvim-treesitter/nvim-treesitter" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "python", "lua", "rust", "vim", "c", "go", "sql" }, -- "all", "maintained" (parsers with maintainers), or list of languages
+				ignore_install = {}, -- List of parsers to ignore installing
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true, -- false will disable the whole extension
+					disable = {}, -- list of language that will be disabled
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true }, -- fix issue with Python indent in new lines
+			})
+		end,
+	},
 	{ "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
 	{ "neovim/nvim-lspconfig" },
 	{ "hrsh7th/nvim-cmp" },
