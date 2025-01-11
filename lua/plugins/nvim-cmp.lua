92ab2125b4ce08b630055bf1f86710ec439f1d0d
Ismael Baira <i.bairao@gmail.com>
2025-01-10 20:01:46 -0800

    Add window to cmp documentation

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -772,6 +772,7 @@ end
 -- Setup nvim-cmp.
 local cmp = require("cmp")
 cmp.setup({
+	window = { documentation = cmp.config.window.bordered() },
 	formatting = {
 		format = function(entry, item)
 			local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
