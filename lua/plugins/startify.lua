fc88ecb4890515d228602d0166dd755e31ae577e
ibaira <i.bairao@gmail.com>
2025-07-12 20:14:48 -0700

    Simplify startify list

--- lua/plugins/startify.lua
+++ lua/plugins/startify.lua
@@ -1,13 +1,7 @@
 -- Startify configuration
 
-local home_path = os.getenv("HOME")
-if home_path == nil then
-	home_path = ""
-end
-
 vim.g.startify_lists = {
 	{ type = "files" },
-	{ type = "dir", header = { "    CWD: " .. vim.fn.getcwd():gsub(home_path, "~") } },
 	{ type = "bookmarks", header = { "    Bookmarks" } },
 }
 
@@ -20,9 +14,9 @@ vim.g.startify_bookmarks = {
 	{ K = "~/.config/nvim/lua/core/keymaps.lua" },
 	{ l = "~/.config/nvim/lua/core/lazy.lua" },
 	{ L = "~/.config/lazygit/config.yml" },
-	{ o = "~/notes/todo.md" },
 	{ r = "~/.config/ruff/pyproject.toml" },
 	{ t = "~/.tmux.conf" },
+	{ T = "~/notes/todo.md" },
 	{ z = "~/.zshrc" },
 }
 
@@ -32,4 +26,3 @@ vim.g.startify_change_to_dir = 0
 vim.g.startify_change_to_vcs_root = 0
 vim.g.startify_fortune_use_unicode = true
 vim.g.startify_custom_header = "startify#pad(startify#fortune#cowsay())"
--- vim.g.startify_custom_header = "startify#pad(startify#fortune#boxed())"
